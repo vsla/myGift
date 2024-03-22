da639b9bff9f6a1ccc777d61e8fbc9c1da5d8ac0
victor attar <vsla@cin.ufpe.br>
2024-03-22 11:56:11 -0300

    Added prisma and sqlite

---
+++ prisma/migrations/20240322145411_devmigration/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "imgUrl" TEXT NOT NULL,
+    "gifted" BOOLEAN NOT NULL DEFAULT false,
+    "giftedBy" TEXT
+);
